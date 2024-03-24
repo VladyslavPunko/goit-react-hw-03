f434ed772f134f3ce3149f2dbe0222b80203a6c0
Mona LisaVladyslav <143653897+VladyslavPunko@users.noreply.github.com>
2024-03-24 23:46:20 +0100

    s

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,11 +1,16 @@
 import css from "./Contact.module.css";
-
+import { FaUser } from "react-icons/fa";
+import { FaPhone } from "react-icons/fa6";
 const Contact = ({ contact }) => {
   return (
     <li className={css.wrapper_contact}>
       <div>
-        <h2>{contact.name}</h2>
-        <p>{contact.number}</p>
+        <h2>
+          <FaUser size="16" /> {contact.name}
+        </h2>
+        <p>
+          <FaPhone size="16" /> {contact.number}
+        </p>
       </div>
       <button type="button" className={css.delete_button}>
         Delete
